1ca46db94a6b1064b96fb424ae1adbd3c4574f9f
yogi1616 <yogesh_barot07@yahoo.com>
2022-01-03 00:10:58 +0530

    blog post added and paytm payment integration done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="blogHome"),
+    path("", views.index, name="blogHome"),
+    path("blogpost/<int:id>", views.blogpost, name="blogHome")
 ]
